3168d8b0bab48f2cac540b37081c82a6ad436309
zubairdv10 <zubairdevries127@gmail.com>
2020-03-09 11:12:02 +0200

    area of circle

---
+++ variables.py
@@ -0,0 +1,14 @@
+a=40
+b=55.6
+c="gold"
+d=3
+print(a,b,c,d)
+e=""""i am learning python basics. 
+Its cool to learn a new language. 
+But hey the weekend was fun"""
+print(e)
+print("4+6")
+pi=3.14
+r=5
+A=(pi * r**2)
+print (A)
